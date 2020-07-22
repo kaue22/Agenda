fdaab90e86510e0e607e1664899a16f734beb53d
kaue <kauegouveia@gmail.com>
2020-07-22 18:34:51 -0300

    Inicio da criação do usuário

--- web/src/database/migrations/1595366709567-CreateAppointments.ts
+++ web/src/database/migrations/1595366709567-CreateAppointments.ts
@@ -16,13 +16,23 @@ export class CreateAppointments1595366709567 implements MigrationInterface {
         {
           name: 'provider',
           type: 'varchar',
-          isNullable: false
+          
         },
         {
           name: 'date',
           type: 'timestamp with time zone',
-          isNullable: false,
-        }]
+          
+        },
+        {
+          name: 'created_at',
+          type: 'timestamp',
+          default:'now()',
+        },
+        {
+          name: 'updated_at',
+          type: 'timestamp',
+          default:'now()',
+        },]
       })
     );
   }
